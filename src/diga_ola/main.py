86dff06c8e82067efc2a0225b373986118c20a40
Diéssica <diessicanaiara072@gmail.com>
2025-06-20 14:54:28 -0300

    firstcommit

---
+++ src/diga_ola/main.py
@@ -0,0 +1,6 @@
+def dizer_ola(nome): 
+    """Retorna ao usuario o nome dele, utilizando o parametro nome"""
+    return f"Ola,{nome}"
+
+"""Imprime  o nome do usuario utilizando a função dizer_ola, com a mensagem Ola parametro"""
+print(dizer_ola("Aluno"))
